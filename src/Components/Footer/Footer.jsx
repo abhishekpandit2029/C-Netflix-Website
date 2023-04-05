78d2d81b7c74899e6d3714eb484bcc11c9fcf60d
abhi <abhishekpandit1212@gmail.com>
2023-04-05 12:12:12 +0530

    bottom signup added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,77 +6,91 @@ import Col from "react-bootstrap/Col";
 
 function Footer() {
   return (
-    <div className="Footer">
-      <p>Questions? Call 000-800-919-1694</p>
-      <Container fluid>
-        <Row className="Row">
-          <Col className="Col" sm={6} md={3}>
-            <ul>
-              <li>
-                <a href="#">FAQ</a>
-              </li>
-              <li>
-                <a href="#">Investor Relations</a>
-              </li>
-              <li>
-                <a href="#">Privacy</a>
-              </li>
-              <li>
-                <a href="#">Speed Test</a>
-              </li>
-            </ul>
-          </Col>
+    <div>
+      <div className="signup_btm">
+        <p className="text_btm">Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className="signup_secondLine_btm">
+          <input
+            className="signup_input_btm"
+            type="Email"
+            placeholder="Email address"
+          />
+          <button className="signup_btn_btm">Get Started {">"} </button>
+        </div>
+      </div>
 
-          <Col className="Col" sm={6} md={3}>
-            <ul>
-              <li>
-                <a href="#">Help Centre</a>
-              </li>
-              <li>
-                <a href="#">Jobs</a>
-              </li>
-              <li>
-                <a href="#">Cookie Preferences</a>
-              </li>
-              <li>
-                <a href="#">Legal Notices</a>
-              </li>
-            </ul>
-          </Col>
-          <Col className="Col" sm={6} md={3}>
-            <ul>
-              <li>
-                <a href="#">Account</a>
-              </li>
-              <li>
-                <a href="#">Ways to Watch</a>
-              </li>
-              <li>
-                <a href="#">Corporate Information</a>
-              </li>
-              <li>
-                <a href="#">Only on Netflix</a>
-              </li>
-            </ul>
-          </Col>
+      <div className="Footer">
+        <p>Questions? Call 000-800-919-1694</p>
+        <Container fluid>
+          <Row className="Row">
+            <Col className="Col" sm={6} md={3}>
+              <ul>
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">Investor Relations</a>
+                </li>
+                <li>
+                  <a href="#">Privacy</a>
+                </li>
+                <li>
+                  <a href="#">Speed Test</a>
+                </li>
+              </ul>
+            </Col>
 
-          <Col className="Col" sm={6} md={3}>
-            <ul>
-              <li>
-                <a href="#">Media Centre</a>
-              </li>
-              <li>
-                <a href="#">Terms of Use</a>
-              </li>
-              <li>
-                <a href="#">Contact Us</a>
-              </li>
-            </ul>
-          </Col>
-        </Row>
-      </Container>
+            <Col className="Col" sm={6} md={3}>
+              <ul>
+                <li>
+                  <a href="#">Help Centre</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+                <li>
+                  <a href="#">Cookie Preferences</a>
+                </li>
+                <li>
+                  <a href="#">Legal Notices</a>
+                </li>
+              </ul>
+            </Col>
+            <Col className="Col" sm={6} md={3}>
+              <ul>
+                <li>
+                  <a href="#">Account</a>
+                </li>
+                <li>
+                  <a href="#">Ways to Watch</a>
+                </li>
+                <li>
+                  <a href="#">Corporate Information</a>
+                </li>
+                <li>
+                  <a href="#">Only on Netflix</a>
+                </li>
+              </ul>
+            </Col>
 
-      <p>Netflix India</p>
+            <Col className="Col" sm={6} md={3}>
+              <ul>
+                <li>
+                  <a href="#">Media Centre</a>
+                </li>
+                <li>
+                  <a href="#">Terms of Use</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+
+        <p>Netflix India</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Favicon from 'react-favicon';
+import n from "../src/Components/Stuff/n.png"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Favicon url={n}/>
     <App />
   </React.StrictMode>
 );
