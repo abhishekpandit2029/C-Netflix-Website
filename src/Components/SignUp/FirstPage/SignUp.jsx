475f8450dff815e1d70524ee8c26478dc147c63f
abhi <abhishekpandit1212@gmail.com>
2023-04-12 15:36:34 +0530

    first signup page displayblock

--- src/Components/SignUp/FirstPage/SignUp.jsx
+++ src/Components/SignUp/FirstPage/SignUp.jsx
@@ -10,7 +10,7 @@ function SignUp() {
             <Navbar /> 
             <div className='signup_container'>
                 <div className="signup_firstLine text-light">
-                    <p className='signup_p1'>Unlimited movies, TV<br/>shows and more.</p>
+                    <p className='signup_p1'>Unlimited movies, TV shows and more.</p>
                     <p className='signup_p2'>Watch anywhere. Cancel anytime.</p>
                     <p className='signup_p3'>Ready to watch? Enter your email to create or restart your membership.</p>
                 </div>
@@ -21,7 +21,7 @@ function SignUp() {
                 </div>
             </div>
         </div> 
-    </div> 
+    </div>  
   )
 }
 
